d7fc075e5f055599f4a9ab50cfe25c1b73111da6
Satyajeet <mohapatras696@gmail.com>
2021-01-01 19:07:06 +0530

    Q1

--- 2041013216/basicjava1/src/basicjava1/Q1.java
+++ 2041013216/basicjava1/src/basicjava1/Q1.java
@@ -3,7 +3,13 @@ package basicjava1;
 public class Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 System.out.println("Hello World");
+	     System.out.println("Hello Again");
+	     System.out.println("I Like Typing This");
+	     System.out.println("This Is Fun");
+	     System.out.println("yay! Frinting");
+	     System.out.println("I had much rather you 'not'");
+	     System.out.println("I \"said\" do not touch this");
 
 	}
 
